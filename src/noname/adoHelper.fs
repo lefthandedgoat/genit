c6f5b95596075e5eea099d45b9ead5c7b2e0615a
Chris Holt <lefthandedgoat@gmail.com>
2016-04-13 21:48:19 -0500

    add adoHelper

---
+++ src/noname/adoHelper.fs
@@ -0,0 +1,48 @@
+module adoHelper
+
+open Npgsql
+
+let firstOrNone s = s |> Seq.tryFind (fun _ -> true)
+
+let boolean (value : string) = System.Convert.ToBoolean(value)
+
+let connection (connectionString : string) =
+  let connection = new NpgsqlConnection(connectionString)
+  connection.Open()
+  connection
+
+let command connection sql =
+  let command = new NpgsqlCommand(sql, connection)
+  command
+
+let param name value (command : NpgsqlCommand) =
+  command.Parameters.AddWithValue(name, value) |> ignore
+  command
+
+let executeScalar (command : NpgsqlCommand) =
+  command.ExecuteScalar()
+
+let executeNonQuery (command : NpgsqlCommand) =
+  command.ExecuteNonQuery() |> ignore
+
+let read toFunc (command : NpgsqlCommand) =
+  use reader = command.ExecuteReader()
+  toFunc reader
+
+let getInt16 name (reader : NpgsqlDataReader) =
+  reader.GetInt16(reader.GetOrdinal(name))
+
+let getInt32 name (reader : NpgsqlDataReader) =
+  reader.GetInt32(reader.GetOrdinal(name))
+
+let getString name (reader : NpgsqlDataReader) =
+  reader.GetString(reader.GetOrdinal(name))
+
+let getDateTime name (reader : NpgsqlDataReader) =
+  reader.GetDateTime(reader.GetOrdinal(name))
+
+let getBool name (reader : NpgsqlDataReader) =
+  reader.GetBoolean(reader.GetOrdinal(name))
+
+let getIntArray name (reader : NpgsqlDataReader) =
+  reader.GetValue(reader.GetOrdinal(name)) :?> int array
