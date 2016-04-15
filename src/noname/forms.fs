3c1269aac6122ed6b9c5e49b8680722d240f62fd
Chris Holt <lefthandedgoat@gmail.com>
2016-04-14 19:36:54 -0500

    remove crap

--- src/noname/forms.fs
+++ src/noname/forms.fs
@@ -1,5 +1,6 @@
 module forms
 
+open Suave
 open Suave.Model.Binding
 open Suave.Form
 open Suave.ServerErrors
@@ -17,50 +18,12 @@ let logAndShow500 error =
 let bindToForm form handler =
   bindReq (bindForm form) handler logAndShow500
 
+let getQueryStringValue (req : HttpRequest) queryStringKey =
+  match (req.queryParam queryStringKey) with
+  | Choice1Of2 value -> value
+  | _ -> ""
 
-type Visibility =
-  | Public
-  | Private
-
-//type RegisterForm =
-//  {
-//    FirstName : string
-//    LastName : string
-//    Email : string
-//    Password : string
-//    RepeatPassword : string
-//    Visibility : string
-//    Age : decimal
-//  }
-//
-//type Register =
-//  {
-//    FirstName : string
-//    LastName : string
-//    Email : string
-//    Password : string
-//    RepeatPassword : string
-//    Visibility : Visibility
-//    Age : int
-//  }
-//
-//let convertRegisterForm (registerForm : RegisterForm) =
-//  {
-//    FirstName = registerForm.FirstName
-//    LastName = registerForm.LastName
-//    Email = registerForm.Email
-//    Password = registerForm.Password
-//    RepeatPassword = registerForm.RepeatPassword
-//    Visibility = fromString<Visibility> registerForm.Visibility
-//    Age = int registerForm.Age
-//  }
-//
-//let registerForm : Form<RegisterForm> = Form ([],[])
-
-type Car =
-  {
-    Make : string
-    Model : string
-    Year : int
-    Price : int
-  }
+let hasQueryString (req : HttpRequest) queryStringKey =
+  match (req.queryParam queryStringKey) with
+  | Choice1Of2 _ -> true
+  | _ -> false
