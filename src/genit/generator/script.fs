9cb68046eca906e69a20d8ddc7c5963214e694c1
Chris Holt <lefthandedgoat@gmail.com>
2016-08-25 21:50:06 -0500

    pass 1

--- src/genit/generator/script.fs
+++ src/genit/generator/script.fs
@@ -9,7 +9,7 @@ let someSite () =
 
   dbPassword "NOTSecure1234"
 
-  site "Bob's Burgers"
+  site "defi Pays"
 
   basic home
 
@@ -17,41 +17,44 @@ let someSite () =
 
   basic login
 
-  page "Order" CVELS
+  page "Self" CVELS
     [
-      text      "Name"          Required
-      text      "Food"          Null
-      text      "Drinks"        Null
-      dollar    "Tip"           Null
-      paragraph "Notes"         Null
-      date      "Delivery Date" Required
-      phone     "Phone Number"  Required
-      text      "Address"       Null
-      text      "City"          Null
-      text      "State"         Null
-      text      "Zip"           Null
-      dropdown  "Free Soda"
-        [
-          "Cola"
-          "Orange"
-          "Root Beer"
-        ]
+      text      "First Name"           Required
+      text      "Last Name"            Required
+      text      "Title"                Required
+      email     "Email"
+      text      "Position Objective 1" Required
+      text      "Position Objective 2" Null
+      text      "Position Objective 3" Null
+      dropdown  "Supervisor"           [ "Sally"; "Bob"; "Pat"; "Tom" ]
+      dropdown  "Coworker or Client 1" [ "Rex"; "Sue" ]
+      dropdown  "Coworker or Client 2" [ "Rex"; "Sue" ]
+      dropdown  "Coworker or Client 3" [ "Rex"; "Sue" ]
+      dropdown  "Coworker or Client 4" [ "Rex"; "Sue" ]
     ]
 
-  api "Order"
-
-  dashboard "Order"
+  advancedPage "Customer" CVEL RequiresLogin
     [
-      line "Delivery Date"
-      bar  "State"
-      pie  "Tip"
+      text  "Customer Name"            Required
+      text  "Contact Name"             Required
+      email "Email"
     ]
 
-  advancedPage "Reserveration" CVELS RequiresLogin
+  page "Employee Survey" CVELS
     [
-      text  "Name"         Required
-      date  "Date"         Required
-      phone "Phone Number" Required
+      text      "Employee Name"        Null
+      paragraph "Position Objective 1" Null
+      paragraph "Position Objective 2" Null
+      paragraph "Position Objective 3" Null
+      dropdown  "Rating"               [ "1"; "2"; "3"; "4"; "5"; "6"; "7"; "8"; "9"; "10"; ]
     ]
+
+  page "Customer Survey" CVELS
+    [
+      text      "Customer"             Null
+      paragraph "Comment"              Required
+      dropdown  "How Likely"           [ "1"; "2"; "3"; "4"; "5"; "6"; "7"; "8"; "9"; "10"; ]
+    ]
+
 
   currentSite
